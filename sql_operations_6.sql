7ae5004098be6baab2a7c25e87b18e4414a9f68a
aishks14 <aishks14@outlook.com>
2025-09-04 19:46:34 +0530

    SQL functions and Operations - Part 2

--- sql_operations_6.sql
+++ sql_operations_6.sql
@@ -1,3 +1,4 @@
+USE sqloperations;
 -- SQL Functions and Operations
 	-- Here we will see some basic operations and functions in SQL
 	-- We will be using the same table 'users' created in previous steps    
@@ -7,55 +8,140 @@
 
     
 		-- COUNT: Counts total number of rows which are matching to the criteria
-		-- COUNT: Get the total count of the users from the `users` table
-		SELECT COUNT(*) AS total_users FROM users;
+			-- COUNT: Get the total count of the users from the `users` table
+			SELECT COUNT(*) AS total_users FROM users;
 		
-		-- COUNT(): Get the total count of the organizations whose industry is `IT Services` or `IT Association`. Query in `organization` table
-		SELECT COUNT(*) FROM organization
-		WHERE industry = 'IT Services' OR industry = 'IT Association';
+			-- COUNT(): Get the total count of the organizations whose industry is `IT Services` or `IT Association`. Query in `organization` table
+			SELECT COUNT(*) FROM organization
+			WHERE industry = 'IT Services' OR industry = 'IT Association';
 		
-		-- COUNT() : Get the total count of the organizations whose industry is `IT Services` or `IT Association` using IN operator. Query in `organization` table
-		SELECT COUNT(*) AS total_it_organizations 
-		FROM organization 
-		WHERE industry IN ('IT Services', 'IT Association');
-		
-		-- MIN(): Gets the minimum value from a range
-		-- MIN(): Get the minimum salary from `users` table
-		SELECT min(salary) FROM users;
+			-- COUNT() : Get the total count of the organizations whose industry is `IT Services` or `IT Association` using IN operator. Query in `organization` table
+			SELECT COUNT(*) AS total_it_organizations 
+			FROM organization 
+			WHERE industry IN ('IT Services', 'IT Association');
 		
 		-- MIN(): Gets the minimum value from a range
-		-- MIN(): Get the minimum salary from `users` table where gender is `Male`
-		SELECT min(salary) FROM users WHERE gender='Male';
+			-- MIN(): Get the minimum salary from `users` table
+			SELECT min(salary) FROM users;
+		
+			-- MIN(): Get the minimum salary from `users` table where gender is `Male`
+			SELECT min(salary) FROM users WHERE gender='Male';
 		
 		-- MAX(): It is an aggregate function of SQL which calculates the maximum value of the range provided.
-		-- MAX(): Get the maximum of total_emp from `organization` table.
-		SELECT MAX(total_emp) FROM organization;
+			-- MAX(): Get the maximum of total_emp from `organization` table.
+			SELECT MAX(total_emp) FROM organization;
 		
 		-- SUM(): I calculates the total of a numeric column which is present in a table
-		-- SUM(): Calculate the sum of budget from table `org_initiatives`
-		SELECT SUM(budget_million) FROM org_initiatives;
+			-- SUM(): Calculate the sum of budget from table `org_initiatives`
+			SELECT SUM(budget_million) FROM org_initiatives;
 		
 		-- AVG(): It computes the average of a numeric column
-		-- AVG(): Calculate the average salary from the `users` table
-		SELECT AVG(salary) FROM users;
+			-- AVG(): Calculate the average salary from the `users` table
+			SELECT AVG(salary) FROM users;
     
     -- STRING functions: These are the functions to combine or modify the text fields
 		-- UPPER(): It converts the string to uppercase
-        -- UPPER(): Convert the names to upper case in `users` table for entire column. This will be using UPDATE query as well
-        UPDATE users
-		SET name = UPPER(name);
+			-- UPPER(): Convert the names to upper case in `users` table for entire column. This will be using UPDATE query as well
+			UPDATE users
+			SET name = UPPER(name);
         
         -- LOWER(): It converts the string to lowercase
-		-- LOWER(): Convert the names to lower case in `users` table for entire column. This will be using UPDATE query as well
-        UPDATE users
-		SET name = LOWER(name);
+			-- LOWER(): Convert the names to lower case in `users` table for entire column. This will be using UPDATE query as well
+			UPDATE users
+			SET name = LOWER(name);
         
-        -- LENGTH(): 
+        -- LENGTH(): This function is used to return the length of the string in bytes
+			-- LENGTH(): Get the length of each email from table `accounts` AS email_length
+			SELECT email, LENGTH(email) AS email_length FROM accounts;
         
-        SHOW Databases;
+			-- LENGTH(): Get all the CEOs from `ceo` table with email longer than 15 characters
+			SELECT * FROM ceo
+			WHERE LENGTH(email) > 15;
         
-        SELECT * FROM organization;
-        SELECT * FROM CEO;
+			-- LENGTH(): Sort all the CEOs data from `ceo` table with their name length in DESCENDING order
+			SELECT *, LENGTH(name) AS name_length FROM ceo
+			ORDER BY LENGTH(name) DESC;
+        
+		-- UPDATE() : Updatw the domain of the email randomly from few records to `@newdomain.com` from `accounts` table
+		UPDATE accounts
+		SET email = CONCAT(SUBSTRING_INDEX(email, '@', 1), '@newdomain.com')
+		WHERE account_id IN(401, 405, 406, 420, 425, 427);
+			
+		-- UPDATE() : Updatw the domain of the email randomly from few records to `@digicrome.com` from `accounts` table
+		UPDATE accounts
+		SET email = CONCAT(SUBSTRING_INDEX(email, '@', 1), '@digicrome.com')
+		WHERE account_id IN(403, 408, 410, 415, 416, 417);
+			
+		-- UPDATE() : Updatw the domain of the email randomly from few records to `@example.in` from `accounts` table
+		UPDATE accounts
+		SET email = CONCAT(SUBSTRING_INDEX(email, '@', 1), '@example.in')
+		WHERE account_id IN(402, 409, 411, 418, 423, 427, 435, 439);
+            
+        -- SUBSTRING_INDEX() : This helps in extracting a part of a string which is based on a delimeter. Useful when spliting the string
+			-- SUBSTRING_INDEX() : Extract domain from email and count frequency
+            SELECT 
+			  SUBSTRING_INDEX(email, '@', -1) AS domain,
+			  COUNT(*) AS domain_count
+			FROM accounts
+			GROUP BY domain
+			ORDER BY domain_count DESC;
+        -- ALTER : Alter table accounts and add a column pan_number with allowed string of 10 characters
+		ALTER TABLE accounts
+		ADD COLUMN pan_number VARCHAR(10);
+         
+		-- FLOOR() : It rounds a number down to the nearest integer
+        -- RAND() : Returns a random float number bewtween 0 and 1
+        -- CHAR() : It converts one or more integer ASCII values into a string
+        -- LPAD() : Pads the left side of a string with a specified character (or string) until it reaches the desired length
+			-- UPDATE : Update the table `accounts` with PAN numbers added in each row
+			UPDATE accounts
+			SET pan_number = CONCAT(
+				CHAR(FLOOR(65 + RAND() * 26)),
+				CHAR(FLOOR(65 + RAND() * 26)),
+				CHAR(FLOOR(65 + RAND() * 26)),
+				CHAR(FLOOR(65 + RAND() * 26)),
+				CHAR(FLOOR(65 + RAND() * 26)),
+				LPAD(FLOOR(RAND() * 10000), 4, '0'),
+				CHAR(FLOOR(65 + RAND() * 26))
+			);
+        
+			-- Validate the PAN number in accounts table
+			SELECT 
+			  full_name,
+			  pan_number,
+			  CASE 
+				WHEN pan_number REGEXP '^[A-Z]{5}[0-9]{4}[A-Z]$' THEN 'Valid'
+				ELSE 'Invalid'
+			  END AS pan_status
+			FROM accounts;
+            
+            -- SPLIT the full name into first_name and last_name from `accounts` table
+            SELECT 
+				SUBSTRING_INDEX(full_name, ' ', 1) AS first_name,
+                SUBSTRING_INDEX(full_name, ' ', -1) AS last_name
+			FROM accounts;
+            
+		
+        -- LENGTH() : counts the actual storage size in bytes. It is useful for planning, data compression, or binary data
+        -- CHAR_LENGTH() : It counts the visible characters. This is ideal for validation, UI display, or text truncation logic
+			-- CHAR_LENGTH() : Find the longest name from `accounts` table
+			SELECT full_name
+			FROM accounts
+			ORDER BY CHAR_LENGTH(full_name) DESC
+			LIMIT 1;
+		
+        -- DATE_FORMAT() : It is a precision tool for turning raw date values into readable, customized strings
+        -- CONCAT() : This is used for joining mutliple strings into one
+			-- CONCAT() : Concatenate multiple fields with formatting
+			SELECT 
+			  CONCAT(full_name, ' (', email, ') - Joined on ', DATE_FORMAT(signup_date, '%d-%b-%Y')) AS user_summary
+			FROM accounts;
+        
+        SHOW tables;
+        
+        SELECT * FROM ceo;
+        
+        SELECT * FROM accounts;
         
         UPDATE CEO
         SET org_id = org_id + 100
